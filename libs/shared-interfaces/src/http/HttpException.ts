a1fd938e782d359487884d5ee5295c4c7ee0cfac
Misha <mishatuohy@protonmail.com>
2022-09-24 14:48:19 +0100

    http exception

---
+++ libs/shared-interfaces/src/http/HttpException.ts
@@ -0,0 +1,3 @@
+export interface HttpException {
+  httpException(status: string, message: string): string;
+}
--- libs/shared-interfaces/src/http/IHttpClient.ts
+++ libs/shared-interfaces/src/http/IHttpClient.ts
@@ -1,6 +1,7 @@
 import { IRequest, IResponse } from '../wrappers';
+import { HttpException } from './HttpException';
 
-export interface IHttpClient {
+export interface IHttpClient extends HttpException {
   get<Type>(request: IRequest<Type>): IResponse<Type>;
   post<Type>(request: IRequest<Type>): IResponse<Type>;
   update<Type>(request: IRequest<Type>): IResponse<Type>;
